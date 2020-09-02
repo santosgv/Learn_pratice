48af62d5cbd13a277b169de02514f007c2709929
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2020-09-02 11:26:06 -0300

    Create calculando_porcentagem.py

---
+++ calculando_porcentagem.py
@@ -0,0 +1,6 @@
+# Calculando porcetagem
+
+porcentagem=float(input('Qual a Porcentage ?'))
+valor=float(input('Qual o valor total ?'))
+total=((valor*porcentagem)/100)
+print('Porcentagem de %.2f e %.2f no total de %.2f'%(porcentagem,total, valor))
